4edb8d4b9f9d3ed2b3a9229c4c60d094bfbc220f
vinky <liwenqi.v@bytedance.com>
2025-05-12 16:36:01 +0800

    feat: 更新图标资源，添加职业、段位和排名图标，并引入新的 SVG 图标

--- src/assets/image/index.ts
+++ src/assets/image/index.ts
@@ -1,3 +1,4 @@
+// 职业图标
 const shaman =
   "https://i.miji.bid/2025/05/10/a3071bddcbfd46bc2224592a684f1d58.png";
 const warlock =
@@ -8,20 +9,18 @@ const deathknight =
   "https://i.miji.bid/2025/05/10/436f9eb5f96c41892ed51207cfb3da0b.png";
 const demonhunter =
   "https://i.miji.bid/2025/05/10/e9a6c85ed7d985c6cc2abb4440d1db0d.png";
-const diamond1 =
-  "https://i.miji.bid/2025/05/10/b238ed55c6ad292c85f401f5bf13a311.png";
-const diamond5 =
-  "https://i.miji.bid/2025/05/10/7c957cb310c1314221250cb5637aaa57.png";
 const druid =
   "https://i.miji.bid/2025/05/10/4fb6c2e89df022a5efa0659ab3b7dfd1.png";
 const hunter =
   "https://i.miji.bid/2025/05/10/c0418b3dbd8df0214ca10fb0ce93ef26.png";
+
+// 段位图标
+const diamond1 =
+  "https://i.miji.bid/2025/05/10/b238ed55c6ad292c85f401f5bf13a311.png";
+const diamond5 =
+  "https://i.miji.bid/2025/05/10/7c957cb310c1314221250cb5637aaa57.png";
 const legend =
   "https://i.miji.bid/2025/05/10/862a61e896312475f76eb36bd1871dfb.png";
-const no1 =
-  "https://i.miji.bid/2025/05/10/e307680c66507c89b7099007ecf07a39.png";
-const no2 =
-  "https://i.miji.bid/2025/05/10/d471f93cad2a7c95f5946a9b1034ebe9.png";
 const paladin =
   "https://i.miji.bid/2025/05/10/acebb55757553d6c364fc0ff31bd9407.png";
 const priest =
@@ -30,13 +29,25 @@ const mage =
   "https://i.miji.bid/2025/05/10/bca31c1290994f4c881f1dd94f1c9ca2.png";
 const rogue =
   "https://i.miji.bid/2025/05/10/78bdb1c94c7e201899e7faff1d945cb9.png";
+// 排名图标
+const no1 =
+  "https://i.miji.bid/2025/05/10/e307680c66507c89b7099007ecf07a39.png";
+const no2 =
+  "https://i.miji.bid/2025/05/10/d471f93cad2a7c95f5946a9b1034ebe9.png";
+
+// 奥数之尘
 const dust = "https://i.imgs.ovh/2025/05/10/vJVCY.webp";
 
 // 游戏模式图标
-const standard = "https://hs.res.netease.com/pc/zt/20241031162506/assets/standard_887a616e.png";
-const wild = "https://hs.res.netease.com/pc/zt/20241031162506/assets/wild_3a6668c1.png";
+const standard =
+  "https://hs.res.netease.com/pc/zt/20241031162506/assets/standard_887a616e.png";
+const wild =
+  "https://hs.res.netease.com/pc/zt/20241031162506/assets/wild_3a6668c1.png";
+const arena =
+  "https://hs.res.netease.com/pc/zt/20241031162506/assets/arena_005138b1.png";
 
 export {
+  arena,
   deathknight,
   demonhunter,
   diamond1,
--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,7 +1,9 @@
+import awards from "./awards.svg";
 import logo from "./logo.svg";
+import piechart from "./piechart.svg";
 import point from "./point.svg";
 import setting from "./setting.svg";
 import sort from "./sort.svg";
 import win from "./win.svg";
 
-export { logo, point, setting, sort, win };
+export { awards,logo, piechart, point, setting, sort, win };
