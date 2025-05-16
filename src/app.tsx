b8f0c7d7b61a94d885a040d9d68c932606a21252
vinky <liwenqi.v@bytedance.com>
2025-05-16 14:28:12 +0800

    feat: 添加应用更新管理功能，提示用户重启以应用新版本

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from 'react';
 
-import { useLaunch } from '@tarojs/taro';
+import Taro, { getUpdateManager, useLaunch } from '@tarojs/taro';
 
 import useSystemInfoStore from '@/store/systemInfo';
 
@@ -8,7 +8,19 @@ import './app.scss';
 
 function App({ children }: PropsWithChildren<any>) {
   useLaunch(() => {
+    const updateManager = getUpdateManager();
     useSystemInfoStore.getState().fetchSystemInfo();
+    updateManager.onUpdateReady(function () {
+      Taro.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            updateManager.applyUpdate();
+          }
+        },
+      });
+    });
   });
 
   return <>{children}</>;
